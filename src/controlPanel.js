01963502b6ee5a6200d1ddec6cc44161b1d70696
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-03-11 19:55:04 +0100

    UI controls encapsulated into ControlPanel class.

---
+++ src/controlPanel.js
@@ -0,0 +1,37 @@
+const Log = require("./log").Log;
+
+class ControlPanel {
+    constructor(controlsIds) {
+        this._blueBotPathInput = document.getElementById(controlsIds.blueBotPathInput);
+        this._redBotPathInput = document.getElementById(controlsIds.redBotPathInput);
+        this._runButton = document.getElementById(controlsIds.runButton);
+        this._blueBotLogTextArea = document.getElementById(controlsIds.blueBotLogTextArea);
+        this._redBotLogTextArea = document.getElementById(controlsIds.redBotLogTextArea);
+
+        this._runButton.addEventListener("click", event => {
+            if (this._runButtonHandler) {
+                this._runButtonHandler();
+            }
+        })
+
+        this._blueBotLog = new Log((message) => {
+            this._blueBotLogTextArea.textContent = this._blueBotLogTextArea.textContent + message;
+            this._blueBotLogTextArea.scrollTop = this._blueBotLogTextArea.scrollHeight;
+        });
+
+        this._redBotLog = new Log((message) => {
+            this._redBotLogTextArea.textContent = this._redBotLogTextArea.textContent + message;
+            this._redBotLogTextArea.scrollTop = this._redBotLogTextArea.scrollHeight;
+        });
+    }
+
+    get blueBotPath() { return this._blueBotPathInput.value; }
+    get redBotPath() { return this._redBotPathInput.value; }
+    set runButtonHandler(handler) { this._runButtonHandler = handler; }
+    get blueBotLog() { return this._blueBotLog; }
+    get redBotLog() { return this._redBotLog; }
+}
+
+module.exports = {
+    ControlPanel: ControlPanel
+}
--- src/renderer.js
+++ src/renderer.js
@@ -9,6 +9,19 @@ const Process = require("./process").Process;
 const TronBot = require("./tronBot").TronBot;
 const TronBotColor = require("./tronBot").TronBotColor;
 const Log = require("./log").Log;
+const ControlPanel = require("./controlPanel").ControlPanel;
+
+const controlPanel = new ControlPanel({
+    blueBotPathInput: "blue-bot-path",
+    redBotPathInput: "red-bot-path",
+    runButton: "run-button",
+    blueBotLogTextArea: "blue-bot-log",
+    redBotLogTextArea: "red-bot-log"
+});
+
+controlPanel._runButtonHandler = () => {
+    console.log(controlPanel.blueBotPath);
+}
 
 let tronString = new TronString("oooooooooooo/oB9o/o10o/o10o/o10o/o10o/o10o/o10o/o10o/o10o/o9Ro/oooooooooooo");
 let tronBoard = new TronBoard(tronString);
@@ -18,13 +31,7 @@ let tronBoardRenderer = new TronBoardRenderer("tron-board-canvas");
 tronBoardRenderer.tronBoard = tronBoard;
 tronBoardRenderer.render();
 
-let redBotLogTextArea = document.getElementById("red-bot-log");
-let redBotLog = new Log(m => {
-    redBotLogTextArea.textContent = redBotLogTextArea.textContent + m;
-    redBotLogTextArea.scrollTop = redBotLogTextArea.scrollHeight;
-})
-
-let tronBot = new TronBot(TronBotColor.Blue, "C:\\Users\\Dawid Komorowski\\source\\repos\\TbiTester\\TbiTester\\bin\\Debug\\TbiTester.exe", redBotLog);
+let tronBot = new TronBot(TronBotColor.Blue, "C:\\Users\\Dawid Komorowski\\source\\repos\\TbiTester\\TbiTester\\bin\\Debug\\TbiTester.exe", controlPanel.blueBotLog);
 
 tronBot.start().then(() => {
     tronBot.makeMove(tronString).then(data => {
